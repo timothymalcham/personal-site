d81a3e2fece670283b685d0ff63cf579f1936ae4
Timothy Malcham <timothymalcham@gmail.com>
2018-03-17 16:54:01 -0700

    adjust styling

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -17,9 +17,8 @@ const TemplateWrapper = ({ children }) => (
     <div
       style={{
         margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        maxWidth: 980,
+        padding: 0
       }}
     >
       {children()}
