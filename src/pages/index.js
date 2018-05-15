ecf91943ea76ab8021a64b9851a804327b038404
Timothy Malcham <timothymalcham@gmail.com>
2018-05-14 20:27:36 -0700

    finish polishing site

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,31 +12,39 @@ const IndexPage = () => (
       I’m Timothy Malcham. I'm a developer who cares about writing high quality code, contributing to well thought out design, and providing users with an excellent experience.
     </p>
 
+    <section>
+      <a className='links' href='https://github.com/timothymalcham'>Github</a>
+      <a className='links' href='https://www.linkedin.com/in/timothy-malcham-16a9823b/'>LinkedIn</a>
+      <a className='links' href='https://drive.google.com/file/d/1hww2T-CTUkgiqHjcyZ-gwP-yvJR_QAfV/view?usp=sharing'>Resume</a>
+    </section>
+
     <p className='section__header'>Work Experience:</p>
 
-    <section className='section__career'>
-      <h2 className='section__title'>Holland America Line</h2>
-      <h3 className='section__sub-title'>Full Stack Developer, July 2016 – March 2018</h3>
-      <p>At Holland America Line I built UI features with Javascript (Angular and React), CSS (PostCSS, LESS, etc.) and worked on APIs/CMS services with Node.js and AEM.</p>
-    </section>
+    <div className='grid'>
+      <section className='section__career'>
+        <h2 className='section__title'>Holland America Line</h2>
+        <h3 className='section__sub-title'>Full Stack Developer, July 2016 – March 2018</h3>
+        <p>At Holland America Line I built UI features with Javascript (Angular and React), CSS (PostCSS, LESS, etc.) and worked on APIs/CMS services with Node.js and AEM.</p>
+      </section>
 
-    <section className='section__career'>
-      <h2 className='section__title'>Design Commission</h2>
-      <h3 className='section__sub-title'>Front End Engineer, May 2014 – July 2016</h3>
-      <p>At Design Commission, I built full-stack and client side web applications with a wide range of technology. I completed projects for a wide range of startups and established companies, working both as a solo developer and with a team.</p>
-    </section>
+      <section className='section__career'>
+        <h2 className='section__title'>Design Commission</h2>
+        <h3 className='section__sub-title'>Front End Engineer, May 2014 – July 2016</h3>
+        <p>At Design Commission, I built full-stack and client side web applications with a wide range of technology. I completed projects for a wide range of startups and established companies, working both as a solo developer and with a team.</p>
+      </section>
 
-    <section className='section__career'>
-      <h2 className='section__title'>Front End Developer (Contractor)</h2>
-      <h3 className='section__sub-title'>Blue Nile, Vitamin T, UP Global, and more, July 2013 – May 2014</h3>
-      <p>I worked as a contract developer at a number of Seattle creative agencies, startups, and a non-profit, using a variety of languages and technologies.</p>
-    </section>
+      <section className='section__career'>
+        <h2 className='section__title'>Front End Developer (Contractor)</h2>
+        <h3 className='section__sub-title'>Blue Nile, Vitamin T, UP Global, and more, July 2013 – May 2014</h3>
+        <p>I worked as a contract developer at a number of Seattle creative agencies, startups, and a non-profit, using a variety of languages and technologies.</p>
+      </section>
 
-    <section className='section__career'>
-      <h2 className='section__title'>Startup Rocket LaunchPad</h2>
-      <h3 className='section__sub-title'>Front End Developer, 2012 – June 2013</h3>
-      <p>Designed and developed responsive web apps and sites for startups, under the mentorship of an experienced developer and entrepreneur.</p>
-    </section>
+      <section className='section__career'>
+        <h2 className='section__title'>Startup Rocket LaunchPad</h2>
+        <h3 className='section__sub-title'>Front End Developer, 2012 – June 2013</h3>
+        <p>Designed and developed responsive web apps and sites for startups, under the mentorship of an experienced developer and entrepreneur.</p>
+      </section>
+    </div>
 
     <p className='section__header'>Education:</p>
 
@@ -50,12 +58,6 @@ const IndexPage = () => (
       <h2 className='section__title'>School of Visual Concepts</h2>
       <h3 className='section__sub-title'>2011 – 2012</h3>
       <p>Studied typography, web design, and UX design.</p>
-    </section>
-
-    <p className='section__header'>Skills:</p>
-
-    <section>
-      <p>JavaScript (React, Redux, Angular.js, Node.js, Webpack, Gulp), HTML5, Handlebars, Pug/Jade, CSS, SCSS, Stylus, LESS, PostCSS. Some experience with: Java, AEM, PHP, Wordpress, Drupal.</p>
     </section>
 
     <p className='section__header'>Selected Work:</p>
